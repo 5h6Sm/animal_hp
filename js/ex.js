6396a42df551bceab87c9ff8aba2bfec8ecccf6a
수민 <5h6sm.github@gmail.com>
2022-11-21 10:34:58 +0900

    입력한 행정시의 동물병원 마커 생성

--- js/ex.js
+++
@@ -1,3 +0,0 @@
-// 주소로 좌표를 검색합니다
-console.log(address);
-
--- js/main.js
+++ js/main.js
@@ -25,16 +25,24 @@ let address = [];
 let printJson = (json) => {
     let check = document.querySelector("#show_hp_list");
     let count = parseInt(json['Animalhosptl'][0]['head'][0]['list_total_count']);
-    let add_li;
+    let hp_name = []; //병원 이름 담는 배열
+    let positions = new Array(); //객체 담는 배열
+
     try {
         if (json['Animalhosptl'][0]['head'][1]['RESULT']['CODE'] == 'INFO-000'){
             for(let i = 0; i<count; i++){
                 if(json['Animalhosptl'][1]['row'][`${i}`]['BSN_STATE_NM'] != '폐업'){
                     try{
                         let insert_li = document.createElement("li");
-                        add_li = check.appendChild(insert_li);
+                        let add_li = check.appendChild(insert_li);
                         add_li.innerText = json['Animalhosptl'][1]['row'][`${i}`]['BIZPLC_NM'];
                         address += json['Animalhosptl'][1]['row'][`${i}`]['REFINE_LOTNO_ADDR'] + ', ';
+                        let addin = {
+                            title : json['Animalhosptl'][1]['row'][`${i}`]['BIZPLC_NM'],
+                            address : json['Animalhosptl'][1]['row'][`${i}`]['REFINE_LOTNO_ADDR'],
+                            latlng: new kakao.maps.LatLng(json['Animalhosptl'][1]['row'][`${i}`]['REFINE_WGS84_LAT'], json['Animalhosptl'][1]['row'][`${i}`]['REFINE_WGS84_LOGT'])
+                        }
+                        positions.push(addin);
                         // let str = json['Animalhosptl'][1]['row'][`${i}`]['BIZPLC_NM'];
                         // check.innerHTML += str + ' ';
                         
@@ -48,64 +56,27 @@ let printJson = (json) => {
         check.innerHTML = "없음";
     }
 
-    // for (let i of address){
-    //     geocoder.addressSearch(`${i}`, function(result, status) {
-    
-    //         // 정상적으로 검색이 완료됐으면 
-    //          if (status === kakao.maps.services.Status.OK) {
-        
-    //             var coords = new kakao.maps.LatLng(lat, lon);
-        
-    //             // 결과값으로 받은 위치를 마커로 표시합니다
-    //             var marker = new kakao.maps.Marker({
-    //                 map: map,
-    //                 position: coords
-    //             });
-        
-    //             // 인포윈도우로 장소에 대한 설명을 표시합니다
-    //             var infowindow = new kakao.maps.InfoWindow({
-    //                 content: '<div style="width:150px;text-align:center;padding:6px 0;">현재위치</div>'
-    //             });
-    //             infowindow.open(map, marker);
-        
-    //             // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-    //             map.setCenter(coords);
-    //         } 
-    //     });    
-        
-    // }
-    
-    var positions = new Array(count);
-
-    for (let i=0; i< count; i++){
-        console.log(check.children[i].innerText);
-        // let m = {
-        //     title : `${add_li[i]}`
-        // };
-        // positions.push(m);
-    }
-    console.log(positions);
-    
+    console.log(positions);    
     
     // 마커 이미지의 이미지 주소입니다
-    // var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
         
-    // for (var i = 0; i < positions.length; i ++) {
+    for (var i = 0; i < positions.length; i ++) {
         
-    //     // 마커 이미지의 이미지 크기 입니다
-    //     var imageSize = new kakao.maps.Size(24, 35); 
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(24, 35); 
         
-    //     // 마커 이미지를 생성합니다    
-    //     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+        // 마커 이미지를 생성합니다    
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
         
-    //     // 마커를 생성합니다
-    //     var marker = new kakao.maps.Marker({
-    //         map: map, // 마커를 표시할 지도
-    //         position: positions[i].latlng, // 마커를 표시할 위치
-    //         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-    //         image : markerImage // 마커 이미지 
-    //     });
-    // }
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지 
+        });
+    }
     
 }
 
